43419a04e94b1aa844c66a83792f13c914c5779c
MaximilianV <code@voelker.dev>
2022-04-06 14:24:01 +0200

    Add a useless start page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Modeler from "../views/Modeler.vue";
+import Overview from "../views/Overview.vue";
 import NotFound from "../views/404.vue";
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Overview",
+    component: Overview,
+    meta: {
+      title: "Overview",
+    },
+  },
   {
     path: "/modeler",
     name: "Modeler",
---
+++ src/views/Overview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-sky-700 text-center text-slate-200 py-12">
+    <h1 class="text-4xl font-bold">Semantic RPA Bot Modeler</h1>
+  </div>
+  <div>
+    <o-button tag="router-link" to="modeler" class="m-4">
+      Open Modeler
+    </o-button>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  name: "overview",
+});
+</script>
+
+<script setup lang="ts">
+import { defineComponent } from "vue";
+</script>
