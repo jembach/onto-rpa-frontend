47e6debb7096c6c99513ef7ad9eaab7b06c02817
Maximilian Voelker <code@voelker.dev>
2024-06-07 10:43:01 +0200

    Change custom top bar to daisyUI navbar

--- src/components/ModelTopBar.vue
+++ src/components/ModelTopBar.vue
@@ -1,27 +1,27 @@
 <template>
-  <div
-    class="bg-sky-700 text-center text-slate-100 py-6 min-h-24 flex-initial flex justify-around shadow-md z-50"
-  >
-    <div class="flex-1">
+  <div class="navbar bg-sky-700 text-slate-100 shadow-md z-50 p-10">
+    <div class="navbar-start">
       <router-link :to="{ name: 'Overview' }" title="Back to Overview">
         <FontAwesomeIcon class="cursor-pointer" :icon="faHouse" size="2xl" />
       </router-link>
       <slot name="left"></slot>
     </div>
 
-    <input
-      v-if="props.modelNameEditable"
-      class="text-center text-white bg-transparent text-4xl border-0 border-b-2 w-4/5 shadow-none"
-      placeholder="Name your new Bot"
-      v-model="modelName"
-    />
-    <div
-      v-else
-      class="text-white bg-transparent text-4xl border-0 border-b-2 w-4/5 shadow-none"
-    >
-      {{ modelName }}
+    <div class="navbar-center w-3/5 justify-center">
+      <input
+        v-if="props.modelNameEditable"
+        class="text-center text-white bg-transparent text-4xl border-0 border-b-2 border-slate-400 w-3/5 shadow-none"
+        placeholder="Name your new Bot"
+        v-model="modelName"
+      />
+      <div
+        v-else
+        class="text-white bg-transparent text-4xl text-center border-0 w-4/5 shadow-none"
+      >
+        {{ modelName }}
+      </div>
     </div>
-    <div class="flex-1">
+    <div class="navbar-end">
       <slot name="right"></slot>
     </div>
   </div>
